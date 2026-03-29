1233be0acdc3dd5a72ba79c9ad18f9df31a6c5fb
Aviv Oron <avivoron@gmail.com>
2026-03-29 22:38:36 +0300

    favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Education from "@/components/Education";
 import Projects from "@/components/Projects";
 
 export const metadata: Metadata = {
-  title: "Aviv Oron",
+  title: "avivo.dev",
 };
 
 export default function Home() {
